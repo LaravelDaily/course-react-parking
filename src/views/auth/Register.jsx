e1bc6a399092a105b9f76c772c6c4f664b78fc31
David Lun <mysticcode@gmail.com>
2023-02-09 18:07:10 +0200

    5. Register page

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,5 +1,85 @@
+import { useState } from 'react'
+
 function Register() {
-  return <div>And that is register view</div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const data = { name, email, password, passwordConfirmation }
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={ handleSubmit } noValidate>
+      <div className="flex flex-col mx-auto md:w-96 w-full">
+
+        <h1 className="heading">Register</h1>
+
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="name" className="required">Name</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            value={ name }
+            onChange={ event => setName(event.target.value) }
+            className="form-input"
+            autoComplete="name"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="email" className="required">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={ email }
+            onChange={ event => setEmail(event.target.value) }
+            className="form-input"
+            autoComplete="email"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4">
+          <label htmlFor="password" className="required">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={ password }
+            onChange={ event => setPassword(event.target.value) }
+            className="form-input"
+            autoComplete="new-password"
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password_confirmation" className="required">Confirm Password</label>
+          <input
+            id="password_confirmation"
+            name="password_confirmation"
+            type="password"
+            value={ passwordConfirmation }
+            onChange={ event => setPasswordConfirmation(event.target.value) }
+            className="form-input"
+            autoComplete="new-password"
+          />
+        </div>
+
+        <div className="border-t h-[1px] my-6"></div>
+
+        <div className="flex flex-col gap-2 mb-4">
+          <button type="submit" className="btn btn-primary">
+            Register
+          </button>
+        </div>
+      </div>
+    </form>
+  )
 }
 
 export default Register
