c682fa67ff95c126d41fcc268d376e4ffd463be8
David Lun <mysticcode@gmail.com>
2023-02-13 16:09:22 +0200

    14. Vehicles list

---
+++ src/hooks/useVehicles.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+
+export function useVehicles() {
+  const [vehicles, setVehicles] = useState([])
+
+  useEffect(() => {
+    const controller = new AbortController()
+    getVehicles({ signal: controller.signal })
+    return () => { controller.abort() }
+  }, [])
+
+  async function getVehicles({ signal } = {}) {
+    return axios.get('vehicles', { signal })
+      .then(response => setVehicles(response.data.data))
+      .catch(() => {})
+  }
+
+  return { vehicles }
+}
--- src/views/vehicles/VehiclesList.jsx
+++ src/views/vehicles/VehiclesList.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 import { route } from '@/routes'
+import { useVehicles } from '@/hooks/useVehicles'
 
 function VehiclesList() {
+  const { vehicles } = useVehicles()
+
   return (
     <div className="flex flex-col mx-auto md:w-96 w-full">
 
@@ -13,7 +16,33 @@ function VehiclesList() {
 
       <div className="border-t h-[1px] my-6"></div>
 
-      <div>There will be vehicles list</div>
+      <div className="flex flex-col gap-2">
+        { vehicles.length > 0 && vehicles.map(vehicle => {
+          return (
+            <div
+              key={ vehicle.id }
+              className="flex bg-gray-100 w-full p-2 justify-between"
+            >
+              <div className="flex items-center overflow-hidden w-full">
+                <div className="text-xl plate">
+                  { vehicle.plate_number }
+                </div>
+                <div className="font-normal text-gray-600 pl-2 grow truncate">
+                  { vehicle.description }
+                </div>
+              </div>
+              <div className="flex gap-1">
+                <button type="button" className="btn btn-secondary text-sm">
+                Edit
+                </button>
+                <button type="button" className="btn text-white bg-red-600 hover:bg-red-500 text-sm">
+                X
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
